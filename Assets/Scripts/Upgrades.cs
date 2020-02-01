747a1de1e6e858c01d725c8fc68728b919e24c2f
David_Goru <david.gomez.rooiz@gmail.com>
2020-02-01 20:46:01 +0100

    Things

--- Assets/Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrades.cs
@@ -5,6 +5,14 @@ public class Upgrades : MonoBehaviour
 {
     public void Add(string type)
     {
-
+        switch(type)
+        {
+            case "Damage":
+                break;
+            case "HP":
+                break;
+            case "Armor":
+                break;
+        }
     }
 }
