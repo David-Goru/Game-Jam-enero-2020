f9b600fdddab493d494ff5488441199f146bfe5c
David_Goru <david.gomez.rooiz@gmail.com>
2020-02-02 01:06:50 +0100

    Psicopompo

---
+++ Assets/Scripts/Psicopompo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Psicopompo : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+        transform.Find("Light").gameObject.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        transform.Find("Light").gameObject.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        GameObject.Find("UI").transform.Find("Upgrades").gameObject.SetActive(true);
+    }
+}
